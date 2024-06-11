f3ac6e566625fdff2d3aff210e0495d8311bdc5d
uaquax <uaquax@tutanota.com>
2024-06-11 21:16:16 +0800

    Remove files

--- src/declarations/types.d.ts
+++ src/declarations/types.d.ts
@@ -1,9 +1,4 @@
-declare module '*.module.scss' {
-  const classes: Record<string, string>;
-  export default classes;
-}
-
-declare module '*.module.scss' {
+declare module '*.module.css' {
   const classes: Record<string, string>;
   export default classes;
 }
