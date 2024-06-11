0cd584903a53fb8daa6e106e04b26c4475bff84b
uaquax <uaquax@tutanota.com>
2024-06-11 20:03:16 +0800

    real names + move icon to a seperate component

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,9 +1,35 @@
 import { useGetProductsQuery } from '../../redux/api/productsApi'
 import s from "./Services.module.css";
 import Service from '../Service/Service';
+import { useEffect, useState } from 'react';
 
 export default function Services() {
-	const { data: services, isError, isLoading } = useGetProductsQuery();
+	const { data, isError, isLoading } = useGetProductsQuery();
+	const [services, setServices] = useState([]);
+
+	useEffect(() => {
+		// Это просто для сходства с макетом, в реальном проекте я бы делал по-другому.
+		if (!data) return;
+
+		const titles = [
+			"СТРАТЕГИЯ",
+			"АНАЛИЗ",
+			"СОЦ СЕТИ",
+			"РАЗВИТИЕ",
+			"КОНТЕНТ",
+			"ДИЗАЙН UX",
+			"ТАРГЕТИРОВАННАЯ РЕКЛАМА",
+			"ПРОДУКТ"
+		];
+		const newList = JSON.parse(JSON.stringify(data.products));
+
+		for (let i = 0; i < titles.length; i++) {
+			if (newList.length - 1 < i) break;
+
+			newList[i].title = titles[i];
+		}
+		setServices(newList);
+	}, [data]);
 
 	if (isError) {
 		return (
@@ -12,10 +38,11 @@ export default function Services() {
 			</div>
 		)
 	}
+
 	return (
 		<>
 			{isLoading ? <div>Loading...</div> : <div className={s.services}>
-				{services?.products?.map((product) => (
+				{services?.map((product) => (
 					<div key={product.id}>
 						<Service product={product} />
 					</div>
